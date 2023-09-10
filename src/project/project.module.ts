396b06d3660824096d1a8276e604c027a08cf0b7
Furkan Doğanay <furkando01@gmail.com>
2023-09-10 13:29:37 +0300

    fix problems

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -4,12 +4,20 @@ import { ProjectController } from './project.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Project } from './entities/project.entity';
 import { ProjectSchema } from './project.schema';
+import { TaskService } from 'src/task/task.service';
+import { Task, TaskSchema } from 'src/task/task.schema';
+import { SolanaService } from 'src/solana/solana.service';
+import { AuthService } from 'src/auth/auth.service';
+import { User, UserSchema } from 'src/auth/user.schema';
+import { AvatarGenerator } from 'src/utils/avatarGenerator';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }]),
+    MongooseModule.forFeature([{ name: Task.name, schema: TaskSchema }]),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [ProjectController],
-  providers: [ProjectService],
+  providers: [ProjectService,TaskService,SolanaService,AuthService,AvatarGenerator],
 })
 export class ProjectModule {}
